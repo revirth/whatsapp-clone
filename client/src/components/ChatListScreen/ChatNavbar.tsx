9bb59de975e9b981ef36cf1c69be77eb55063e68
JT <revirth@gmail.com>
2019-06-14 14:51:25 -0400

    Material UI, styled-components

--- client/src/components/ChatListScreen/ChatNavbar.tsx
+++ client/src/components/ChatListScreen/ChatNavbar.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { Toolbar } from "@material-ui/core";
+import styled from "styled-components";
 
-const ChatNavbar: React.FC = () => <div>Whatsapp Clone</div>;
+const Container = styled(Toolbar)`
+  background-color: var(--primary-bg);
+  color: var(--primary-text);
+  font-size: 20px;
+  line-height: 40px;
+` as typeof Toolbar;
+
+const ChatNavbar: React.FC = () => <Container>Whatsapp Clone</Container>;
 
 export default ChatNavbar;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: "#2c6157" },
+    secondary: { main: "#6fd056" }
+  }
+});
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <MuiThemeProvider theme={theme}>
+    <App />
+  </MuiThemeProvider>,
+  document.getElementById("root")
+);
